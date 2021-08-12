9ce501f00eefecb17e3df1540527c7d54a7ed2b6
haalqershi <49689391+haalqershi@users.noreply.github.com>
2021-08-12 13:07:31 -0400

    delete

--- src/main/java/com/hesham/backend/security/UserAuthorities.java
+++
@@ -1,6 +0,0 @@
-package com.hesham.backend.security;
-
-public class UserAuthorities {
-    public static final String[] ADMIN_AUTHORITIES = {"user:read", "user:update", "user:delete", "user:create"};
-    public static final String[] USER_AUTHORITIES = {"user:read", "user:update"};
-}
