7728960c88c8022f580ef3abc9b2374e6278843b
haalqershi <49689391+haalqershi@users.noreply.github.com>
2021-09-01 19:05:34 -0400

    adding Address Entity

---
+++ src/main/java/com/hesham/backend/model/Address.java
@@ -0,0 +1,28 @@
+package com.hesham.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String zipCode;
+
+}
