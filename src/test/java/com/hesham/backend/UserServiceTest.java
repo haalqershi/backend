ec9a40d13877ecc87cd9948172f55c1d101365ac
haalqershi <49689391+haalqershi@users.noreply.github.com>
2021-09-08 23:33:25 -0400

    Adding get all users unit test

--- src/test/java/com/hesham/backend/UserServiceTest.java
+++ src/test/java/com/hesham/backend/UserServiceTest.java
@@ -5,6 +5,7 @@ import com.hesham.backend.repository.UserRepository;
 import com.hesham.backend.service.UserService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +13,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest
@@ -39,5 +45,25 @@ public class UserServiceTest {
     @AfterEach
     public void after(){
         user = null;
+    }
+
+    @Test
+    public void testGetAllUsers(){
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+
+        when(userRepository.findAll()).thenReturn(userList);
+
+        List<User> users = userService.getAllUsers();
+        assertEquals(users.size(), 1);
+        assertEquals(users.get(0).getFirstName(), "Hesham");
+
+        userList.add(new User(1l,"John", "Smith", "john123"
+                , "john123@hcl.com", "123", null, true
+                , true, new Date(), null));
+
+        users = userService.getAllUsers();
+        assertEquals(users.size(), 2);
+        assertEquals(users.get(1).getFirstName(), "John");
     }
 }
