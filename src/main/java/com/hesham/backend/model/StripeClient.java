ed83e38680329b77a0cc08ef143bda876a0f1d6b
haalqershi <49689391+haalqershi@users.noreply.github.com>
2021-09-13 11:03:26 -0400

    fixing stripeClient code smell

--- src/main/java/com/hesham/backend/model/StripeClient.java
+++ src/main/java/com/hesham/backend/model/StripeClient.java
@@ -1,6 +1,7 @@
 package com.hesham.backend.model;
 
 import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
 import com.stripe.model.Charge;
 import com.stripe.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,34 +13,34 @@ import java.util.Map;
 @Component
 public class StripeClient {
 
+    private final String SOURCE = "source";
+
     @Autowired
     StripeClient() {
         Stripe.apiKey = "sk_test_51JUZpRD3Qqgxv1SjLZPUXIUCXm6mC8VeIfQcz6MhfGi4UZIzhyoxYA2AGX3sawLA1Zu6ZJOFT9ejOQp1VUGqHYLv00Vbc5ECXr";
     }
 
-    public Charge chargeCreditCard(String token, double amount) throws Exception {
-        Map<String, Object> chargeParams = new HashMap<String, Object>();
+    public Charge chargeCreditCard(String token, double amount) throws StripeException {
+        Map<String, Object> chargeParams = new HashMap<>();
         chargeParams.put("amount", (int)amount);
         chargeParams.put("currency", "USD");
-        chargeParams.put("source", token);
-        Charge charge = Charge.create(chargeParams);
-        return charge;
+        chargeParams.put(SOURCE, token);
+        return  Charge.create(chargeParams);
     }
-    public Customer createCustomer(String token, String email) throws Exception {
-        Map<String, Object> customerParams = new HashMap<String, Object>();
+    public Customer createCustomer(String token, String email) throws StripeException {
+        Map<String, Object> customerParams = new HashMap<>();
         customerParams.put("email", email);
-        customerParams.put("source", token);
+        customerParams.put(SOURCE, token);
         return Customer.create(customerParams);
     }
-    public Charge chargeCustomerCard(String customerId, int amount) throws Exception {
-        String sourceCard =     Customer.retrieve(customerId).getDefaultSource();
-        Map<String, Object> chargeParams = new HashMap<String, Object>();
+    public Charge chargeCustomerCard(String customerId, int amount) throws StripeException {
+        String sourceCard = Customer.retrieve(customerId).getDefaultSource();
+        Map<String, Object> chargeParams = new HashMap<>();
         chargeParams.put("amount", amount);
         chargeParams.put("currency", "USD");
         chargeParams.put("customer", customerId);
-        chargeParams.put("source", sourceCard);
-        Charge charge = Charge.create(chargeParams);
-        return charge;
+        chargeParams.put(SOURCE, sourceCard);
+        return Charge.create(chargeParams);
     }
 
 }
